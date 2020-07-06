0428d1c249eb422469ffce37f5283453380fa048
PrabhaMuthu <prabhamuthu@gmail.com>
2020-07-06 18:40:11 +0100

    Added The JunitReport

---
+++ GitSCMDemo/src/test/java/JunitReport.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class JunitReport {
+
+    @Test
+     public void test1(){
+        System.out.println("The first test");
+    }
+    @Test
+    public void tes2(){
+        System.out.println("The second Test");
+    }
+
+}
