6fee6991d3f2c8b4607f0f4f9ea010d090b902d7
PrabhaMuthu <prabhamuthu@gmail.com>
2020-07-08 22:41:29 +0100

    version 1.0

---
+++ GitSCMJenkinExample/src/main/java/uk/prabha/DevInTest/JenkinDemo.java
@@ -0,0 +1,20 @@
+
+package uk.prabha.DevInTest;
+
+    /* To push project in github repa and create the jenkin task to run
+    and publish the junit reports
+     */
+    public class JenkinDemo {
+
+        public static void main (String[] args) {
+            System.out.println( "The first version of the Github Repo ");
+            System.out.println("To create a jenkin taks");
+            System.out.println("test");
+
+        }
+    }
+
+
+
+
+
