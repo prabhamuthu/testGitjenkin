1be1aaca3fc2c58fbca8af3833af8d8d191e7008
PrabhaMuthu <prabhamuthu@gmail.com>
2020-07-08 21:52:45 +0100

    commit the githubscmdemo

---
+++ GitSCMDemo/src/main/java/uk/prabha/DevInTest/SourceControl.java
@@ -0,0 +1,14 @@
+package uk.prabha.DevInTest;
+
+/* To push project in github repa and create the jenkin task to run
+and publish the junit reports
+ */
+public class SourceControl {
+
+    public static void main (String[] args) {
+        System.out.println( "The first version of the Github Repo ");
+        System.out.println("To create a jenkin taks");
+        System.out.println("test");
+
+    }
+}
